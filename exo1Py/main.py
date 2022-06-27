e0be62779de2c9d55403d6c33ef6f1ad8c21dc22
Paul Geneve <geneve.paul1295@gmail.com>
2022-06-27 10:57:40 +0200

    refracto

--- exo1Py/main.py
+++ exo1Py/main.py
@@ -1,11 +1,9 @@
-a = 1
+bool = True
 
-while a > 0:
+while bool:
     a = int(input('Entrez une année : '))
     if( a%4==0 and a%100!=0 or a%400==0):
         print("L'annee est une annee bissextile!")
     else:
         print("L'annee n'est pas une annee bissextile!")
-
-
 
